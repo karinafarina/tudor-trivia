cb4655be96b7329a359419240f0d4431d0d9ced8
Karina Gaulin <karina@domandtom.com>
2019-05-16 12:12:09 -0400

    add store

---
+++ dataStore.js
@@ -0,0 +1,116 @@
+const STORE = [
+  {
+    question: 'Who was the fourth wife of King Henry the 8th?',
+    answers: [
+      'Anne Boleyn',
+      'Anne of Cleeves',
+      'Catherine Howard',
+      'Katherine Parr'
+    ],
+    correctAnswer: 'Anne of Cleeves',
+    img: 'img/anneOfCleves.jpg',
+    alt: 'Portrait of Anne of Cleves'
+  },
+  {
+    question: 'How many living children did his first wife have?',
+    answers: [
+      3,
+      8,
+      0,
+      1
+    ],
+    correctAnswer: 1,
+    img: 'img/maryTudor.jpg',
+    alt: 'Portrait of Mary Tudor'
+  },
+  {
+    question: 'How did Catherine Howard die?',
+    answers: [
+      'Childbirth',
+      'Natural causes',
+      'Beheading',
+      'The Plague'
+    ],
+    correctAnswer: 'Beheading',
+    img: 'img/catherineExecution.jpg',
+    alt: "Drawing of Catherine Howard's Execution"
+  },
+  {
+    question: 'Who was the successor to Queen Elizabeth the 1rst?',
+    answers: [
+      'Her son, Henry the 9th',
+      'Her daughter, Elizabeth the 2nd',
+      'Her first cousin, once removed, Mary, Queen of Scots',
+      'Her first cousin, twice removed, James Stewart'
+    ],
+    correctAnswer: 'Her first cousin, twice removed, James Stewart',
+    img: 'img/kingJames.jpg',
+    alt: 'Image of King James the first of England'
+  },
+  {
+    question: 'What common ancestor did Queen Elizabeth and James Stewart share?',
+    answers: [
+      'King Henry the 5th',
+      'King Henry the 8th',
+      'King Henry the 7th',
+      'Queen Anne Bolyen
+    ],
+    correctAnswer: 'King Henry the 7th',
+    img: 'img/henryTheSeventh.jpg',
+    alt: 'Portrait of King Henry the 7th'
+  },
+  {
+    question: 'How many of King Henry the 8th’s six wives survived their marriages to him?',
+    answers: [
+      1,
+      0,
+      2,
+      5
+    ],
+    correctAnswer: 2,
+    img: [
+      'img/anneOfCleves.jpg',
+      'img/katherineParr.jpg'
+    ],
+    alt: [
+      'Portrait of Anne of Cleves',
+      'Portrait of Katherine Parr'
+    ]
+  },
+  {
+    question:'Who was King Henry the 8th’s mother?',
+    answers: [
+      'Queen Elizabeth the 1st',
+      'Elizabeth of York',
+      'Queen Elizabeth the 2nd',
+      'Elizabeth Woodville'
+    ],
+    correctAnswer: 'Elizabeth of York',
+    img: 'img/elizabethYork.jpg',
+    alt: 'Portrait of Elizabeth of York',
+  },
+  {
+    question: "Which of Henry the 8th's six wives had a living son?",
+    answers: [
+      'Anne Boleyn',
+      'Anne of Cleeves',
+      'Jane Seymore',
+      'Katherine Parr'
+    ],
+    correctAnswer: 'Jane Seymoure',
+    img: 'img/janeSeymour.jpg',
+    alt: 'Portrait of Jane Seymour'
+  },
+  {
+    question: "Who was King Henry the 8th's successor?",
+    answers: [
+      'Queen Elizabeth the 1st',
+      'Queen Mary the 1st',
+      'King Edward the 6th',
+      'Queen Victoria the 1st'
+    ],
+    correctAnswer: 'King Edward the 6th',
+    img: 'img/edwardVI.jpg',
+    alt: 'Portrait of King Edward the 6th'
+  },
+]
