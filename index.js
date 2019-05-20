54c87250d31b7a4f665ecf2aeb6dc5b56dcacd8e
Karina Gaulin <karina@domandtom.com>
2019-05-20 10:47:01 -0400

    Working on handleRenderQuestion

--- index.js
+++ index.js
@@ -3,10 +3,48 @@
 let questionNumber = 0;
 let tally = 0;
 
+//On click of the start button, remove the home view and add the questions view
+function handleStartButton() {
+  $('main').on('click', '.start', function() {
+    $('.start-screen').remove();
+    $('.questions').css('display', 'flex');
+    $('.question-number').text(1);
+    handleRenderQuestion();
+  });
+  console.log('`handleStartButton` ran');
+}
+
+//Loop through and Display each question from the STORE and the choices with a submit button
+function generateQuestions() {
+  if(questionNumber < STORE.length) {
+    return
+    `<div class="question-${questionNumber}">
+      <h1 class="question-title">${STORE[questionNumber].question}</h1>
+        <form>
+          <input type="radio" name="a" value="Anne Boleyn" checked="true">Anne Boleyn<br>
+          <input type="radio" name="b" value="Anne of Cleves">Anne of Cleves<br>
+          <input type="radio" name="c" value="Catherine Howard">Catherine Howard<br>
+          <input type="radio" name="d" value="Katherine Parr">Katherine Parr<br />
+          <button type="submit" name="submit" class="submit" value="Submit">Submit</button>
+        </form>`;
+  } else {
+    overallScore();
+    handleStartNewButton();
+  }
+  console.log('`generateQuestions` ran');
+}
+
+
+function handleRenderQuestion() {
+  $('.questions').html(generateQuestions());
+  console.log('`handleRenderQuestion()` ran')
+}
+
 //increment question number for each question
 function changeQuestionNumber() {
   questionNumber ++;
   console.log(questionNumber);
+  $('.question-number').text(questionNumber + 1);
 }
 
 //increment score
@@ -15,43 +53,21 @@ function changeTally() {
   console.log(tally);
 }
 
-//On click of the start button, remove the home view and add the questions view
-function startQuiz() {
-  $('main').on('click', '.start', function() {
-    $('.start-screen').remove();
-    generateQuestions();
-  });
-  console.log('`startQuiz` ran');
-}
-
-function generateQuestions() {
-  //Loop through and Display each question from the STORE and the choices with a submit button
-  //one question at a time
-  //Make another function to update the number and number correct
-  if(questionNumber < STORE.length) {
-    return `<section class="questions">
-      <h1 class="question-1-title">${STORE[questionNumber].question}</h1>
-      <form>
-        <input type="radio" name="a" value="Anne Boleyn" checked="true">Anne Boleyn<br>
-        <input type="radio" name="b" value="Anne of Cleves">Anne of Cleves<br>
-        <input type="radio" name="c" value="Catherine Howard">Catherine Howard<br>
-        <input type="radio" name="d" value="Katherine Parr">Katherine Parr<br />
-        <a href="./answers.html"><button type="submit" name="submit" class="submit" value="Submit">Submit</a>
-      </form>
-    </section>`;
-  } else {
-    overallScore();
-    startNew();
-  }
-  console.log('`generateQuestions` ran');
-}
-
-function correctIncorrect() {
+function handleSubmitButton(data) {
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    let selected = $('input:checked');
+    let userAnswer = selected.val();
+    let correctAnswer = `${data[questionNumber].correctAnswer}`;
+    if(correctAnswer === userAnswer) {
+      console.log('that is correct');
+    }
+  })
   //if answer is correct, render html for correct statement with image and alt
   changeTally();
   changeQuestionNumber();
   //if answer is incorrect, render html for incorrect statement with correct statement and image and alt
-  console.log('`correctIncorrect` ran')
+  console.log('`handleSubmitButton` ran')
   changeQuestionNumber();
 
 }
@@ -61,16 +77,16 @@ function overallScore() {
   console.log('`overallScore` ran');
 }
 
-function startNew() {
+function handleStartNewButton() {
   //start a new quiz
-  console.log('`startNew` ran');
+  console.log('`handleStartNewButton` ran');
 }
 
 function handleQuizApp() {
-  startQuiz();
-  correctIncorrect();
-  overallScore();
-  startNew();
+  handleStartButton();
+  handleRenderQuestion();
+  //handleSubmitButton();
+  //handleStartNewButton();
 }
 
 
